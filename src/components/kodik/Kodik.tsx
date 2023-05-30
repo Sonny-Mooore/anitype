f7903fedd84eedf8ab9d4b8349bba13d91824ecb
nnveter <nnveter1@gmail.com>
2023-05-30 13:15:25 +0300

    + kodik skip button

--- src/components/kodik/Kodik.tsx
+++ src/components/kodik/Kodik.tsx
@@ -1,5 +1,5 @@
 "use client"
-import React, {useEffect} from 'react';
+import React, {useEffect, useRef} from 'react';
 import "./kodik.css";
 interface KodikProps{
     src: string
@@ -7,26 +7,30 @@ interface KodikProps{
 
 const Kodik = ({src}: KodikProps) => {
 
+    const time= useRef(0)
+
     function setEpisodeAndSeason(season:number, episode: number){
-        sendMethodKodik("kodik-player", {method: "seek", seconds: {method: "change_episode", season: season, episode: episode}})
+        sendMethodKodik({method: "change_episode", seconds: {method: "change_episode", season: season, episode: episode}})
     }
 
-    function setTime(time:number){
-        sendMethodKodik("kodik-player", {method: "seek", seconds: time})
+    function setTime(time: number){
+        sendMethodKodik( {method: "seek", seconds: time})
     }
 
-    function sendMethodKodik(key: string, value: any){
+    function sendMethodKodik(value: any){
         // @ts-ignore
         let kodikIframe = document.getElementById("kodik-player")?.contentWindow;
         if (kodikIframe){
-            kodikIframe.postMessage({ key: key, value: value}, '*');
+            console.log({ key: "kodik_player_api", value: value})
+            kodikIframe.postMessage({ key: "kodik_player_api", value: value}, '*');
         }
     }
 
     useEffect(() => {
         const kodikMessageListener = (message: any) => {
             if (message.data.key === 'kodik_player_time_update') {
-                console.log(message.data.value);
+                time.current = message.data.value
+                console.log(time.current)
             }
             if (message.data.key === "kodik_player_current_episode"){
                 console.log(message.data.value);
@@ -51,7 +55,14 @@ const Kodik = ({src}: KodikProps) => {
     }, []);
 
     return (
-        <iframe style={{border: "none"}} allowFullScreen allow="autoplay ; fullscreen" width={"100%"} height={"100%"} src={src} />
+        <>
+            <div className={"kodik_close_button"} onClick={() => setTime(time.current + 80)}>
+                <svg height="20px" width="20px" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M22.2,10.6l-9-5.4c-1-0.6-2.2,0.2-2.2,1.4v3.2L3.2,5.2C2.2,4.6,1,5.4,1,6.6v10.7c0,1.2,1.2,2,2.2,1.4l7.8-4.6   v3.2c0,1.2,1.2,2,2.2,1.4l9-5.4C23.3,12.8,23.3,11.2,22.2,10.6z"/>
+                </svg>
+            </div>
+            <iframe id="kodik-player" style={{border: "none"}} allowFullScreen allow="autoplay ; fullscreen" width={"100%"} height={"100%"} src={src} />
+        </>
     );
 };
 
