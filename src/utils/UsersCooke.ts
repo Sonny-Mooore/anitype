928c6d3b8502d4535bf18eea945b162aa8ea6f9f
nnveter <nnveter1@gmail.com>
2023-06-06 22:29:34 +0300

    add email verification (not completed yet) add user info in account dialog

--- src/utils/UsersCooke.ts
+++ src/utils/UsersCooke.ts
@@ -1,4 +1,9 @@
-import {deleteCookie, getCookie, setCookie} from "cookies-next";
+import {deleteCookie, getCookie, hasCookie, setCookie} from "cookies-next";
+import {UserInfo} from "@/utils/interfaces";
+import axios from "axios";
+import {URLUsers} from "@/utils/constants";
+import {getJwt} from "@/utils/JWT";
+
 export function setUserName(name: string) {
     setCookie('userName', name, {
         maxAge: 86400 * 7,
@@ -11,6 +16,37 @@ export function getUserName(): string {
 
 export function clearUserInfo() {
     deleteCookie('userName')
+    localStorage.removeItem("UserInfo")
+    deleteCookie("UserInfoIsNotOutdated")
 }
+
+export function setUserInfo(userInfo: UserInfo){
+    setUserName(userInfo.username)
+    localStorage.setItem('UserInfo', JSON.stringify(userInfo));
+    setCookie("UserInfoIsNotOutdated", true, {maxAge:  600})
+}
+
+export async function getUserInfo(): Promise<UserInfo | undefined>{
+    if (hasCookie('UserInfoIsNotOutdated')){
+        const storedObject = localStorage.getItem('UserInfo');
+        if (storedObject) {
+            return JSON.parse(storedObject)
+        }
+    }else{
+        axios({
+            method: "get",
+            url: URLUsers + "/users/my/info",
+            headers: {"Authorization": "Bearer " + (await getJwt()).access}
+        }).then(res => {
+            setUserInfo(res.data)
+            return res.data
+        }).catch(e => {
+            console.log(e)
+        })
+    }
+
+}
+
+
 
 //users/my/info
